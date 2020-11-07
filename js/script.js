0c6db3ffde7a4f41a72a72e154fcda7312782755
Ommika <51721991+Ommika@users.noreply.github.com>
2020-11-08 01:24:08 +0300

    Reloading some files

--- js/script.js
+++ js/script.js
@@ -1,9 +1,47 @@
 $(document).ready(function () {
 
-    $('.main_btna').on('click', function () {
+    function openModal() {
         $('.overlay').fadeIn(500);
         $('.modal').show(500);
+    }
+
+    function closeModal() {
+
+        if ($('.modal').css('display') == 'block') {
+            $('.modal').hide(500);
+            $('.overlay').fadeOut(500);
+        } else if ($('.thanks').css('display') == 'block') {
+            $('.thanks').hide(500);
+            $('.overlay').fadeOut(500);
+        } else {
+            console.log("Что-то пошло не так!");
+        }
+
+    }
+
+    $('#form').on('submit', function (event) {
+        event.preventDefault();
+        $('.modal').hide(500);
+        $('.thanks').show(500);
     });
+
+    function closeThanks() {
+        $('.thanks').hide();
+        $('.overlay').fadeOut(500);
+    }
+
+    $('.main_btna').on('click', openModal);
+    $('.main_btn').on('click', openModal);
+    $('.overlay').on('click', closeModal);
+    $('.close:first').on('click', closeModal);
+    $('.back').on('click', closeThanks);
+
+
+    // Disable #x
+    // $(".submit").prop("disabled", true);
+
+    // Enable #x
+    // $("#x").prop("disabled", false);
 
 
 });
